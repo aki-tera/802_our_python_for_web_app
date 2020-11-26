0fed1ac0ef1a68c559c3580cc9ebceed87098739
aki-tera <aki.tera@gmail.com>
2020-11-26 09:47:17 +0900

    2020/11/26

--- test05/cgi-bin/templatebbs.py
+++ test05/cgi-bin/templatebbs.py
@@ -19,7 +19,7 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
 con = sqlite3.connect("./bookmark.dat")
 cur = con.cursor()
 try:
-    cur.execute("""CREATE TABLE bookmark ( titel text, url text);""")
+    cur.execute("""CREATE TABLE bookmark (title text, url text);""")
 except BaseException:
     pass
 
@@ -27,24 +27,32 @@ req = Request()
 f = req.form
 
 value_dic = {"message": "", "title": "", "url": "", "bookmarks": ""}
+for key in value_dic:
+    value_dic[key] = f.getvalue(key, "")
 
 if "post" in f:
     if not f.getvalue("title", "") or not f.getvalue("url", ""):
         value_dic["message"] = "タイトルとURLは必須項目です"
-        value_dic["title"] = f.getvalue("titel", "")
-        value_dic["url"] = f.getvalue("url", "")
     else:
+        res = Response()
+        body = ""
+        for word, read in value_dic.items():
+            body = body + word + ":" + read + "**  **"
+        res.set_body(body)
+        print(res)
         cur.execute(
-            """INSERT INTO bookmark(titel, url) VALUES(?, ?)""", f.getvalue(
-                "title", ""), f.getvalue(
-                "url", ""))
+            """INSERT INTO bookmark (title, url) VALUES(?, ?)""", (
+                value_dic["title"],
+                value_dic["url"])
+        )
         con.commit()
+
 
 res = Response()
 
 with open(path.join(path.dirname(".."), "bookmarkform.html"), encoding="utf-8") as f:
-    #res.set_body(path.join(os.path.abspath(path.dirname("..")), "bookmarkform.html") + "test")
-    #print(res)
+    # res.set_body(path.join(os.path.abspath(path.dirname("..")), "bookmarkform.html") + "test")
+    # print(res)
     t = Template(f.read())
     body = t.substitute(value_dic)
 
