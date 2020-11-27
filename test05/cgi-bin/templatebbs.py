ff33e12340bb82227d80c2cacb49370d628669f1
aki-tera <aki.tera@gmail.com>
2020-11-27 10:01:18 +0900

    2020/11/27

--- test05/cgi-bin/templatebbs.py
+++ test05/cgi-bin/templatebbs.py
@@ -25,34 +25,36 @@ except BaseException:
 
 req = Request()
 f = req.form
-
 value_dic = {"message": "", "title": "", "url": "", "bookmarks": ""}
+
+# 辞書に値を入れるように追加
 for key in value_dic:
     value_dic[key] = f.getvalue(key, "")
+
+# デバッグ用
+res = Response()
+body = ""
+for word, read in value_dic.items():
+    body = body + word + ":" + read + "**  **"
+res.set_body(body)
+print(res)
 
 if "post" in f:
     if not f.getvalue("title", "") or not f.getvalue("url", ""):
         value_dic["message"] = "タイトルとURLは必須項目です"
     else:
-        res = Response()
-        body = ""
-        for word, read in value_dic.items():
-            body = body + word + ":" + read + "**  **"
-        res.set_body(body)
-        print(res)
+        # 現在の問題点は文字入力ができないこと
         cur.execute(
-            """INSERT INTO bookmark (title, url) VALUES(?, ?)""", (
+            """INSERT INTO bookmark(title, url) VALUES({0}, {1})""".format(
                 value_dic["title"],
-                value_dic["url"])
-        )
+                str(value_dic["url"])))
         con.commit()
 
 
 res = Response()
 
+# with構文に変更
 with open(path.join(path.dirname(".."), "bookmarkform.html"), encoding="utf-8") as f:
-    # res.set_body(path.join(os.path.abspath(path.dirname("..")), "bookmarkform.html") + "test")
-    # print(res)
     t = Template(f.read())
     body = t.substitute(value_dic)
 
