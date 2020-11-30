58a3da9a180ef95cd32a04b46db6f5176f213eb1
aki-tera <aki.tera@gmail.com>
2020-11-30 09:45:14 +0900

    2020/11/30 ・sqlのエラーは、テキストをINSERTする際は＇＇でくくる必要あり ・『標準モジュールを使ったテンプレートエンジン』を終了

--- test05/cgi-bin/templatebbs.py
+++ test05/cgi-bin/templatebbs.py
@@ -3,10 +3,10 @@ import io
 import sqlite3
 from string import Template
 from os import path
-import os
+
 
 # 前回作ったプログラムをインポートする
-from httphandler import Request, Response, get_htmltemplate
+from httphandler import Request, Response
 
 
 # デバッグ表示
@@ -31,27 +31,37 @@ value_dic = {"message": "", "title": "", "url": "", "bookmarks": ""}
 for key in value_dic:
     value_dic[key] = f.getvalue(key, "")
 
-# デバッグ用
+# デバッグ用：開始
 res = Response()
 body = ""
 for word, read in value_dic.items():
     body = body + word + ":" + read + "**  **"
 res.set_body(body)
 print(res)
+# デバッグ用：終了
 
 if "post" in f:
     if not f.getvalue("title", "") or not f.getvalue("url", ""):
         value_dic["message"] = "タイトルとURLは必須項目です"
     else:
-        # 現在の問題点は文字入力ができないこと
+        # INSERTする際の文字列は’’でくるらないとエラーが発生する
         cur.execute(
-            """INSERT INTO bookmark(title, url) VALUES({0}, {1})""".format(
+            """INSERT INTO bookmark(title, url) VALUES('{0}', '{1}')""".format(
                 value_dic["title"],
-                str(value_dic["url"])))
+                value_dic["url"]))
         con.commit()
 
 
 res = Response()
+
+listbody = ""
+cur.execute("SELECT title, url FROM bookmark")
+for item in cur.fetchall():
+    # タプルで出力されるのでスライスする
+    listbody += """<dt>{0}</dt><dd>{1}</dd>\n""".format(item[0], item[1])
+listbody = """<ul>\n{0}</ul>""".format(listbody)
+value_dic["bookmarks"] = listbody
+
 
 # with構文に変更
 with open(path.join(path.dirname(".."), "bookmarkform.html"), encoding="utf-8") as f:
